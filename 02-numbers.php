bec5fbe1c4e4595c71c161743397262206313350
f-gehin-crm <183800959+f-gehin-crm@users.noreply.github.com>
2025-10-27 12:22:00 +0100

    division et multiplication

--- 02-numbers.php
+++ 02-numbers.php
@@ -5,7 +5,15 @@
 * @param int $a le premier entier
 * @param int $b le deuxième entier
 * @return int la somme des deux entiers
+* @param int $a le premier entier
+* @param float $c le premier float
+* @param float $d le deuxième float
+* @return float le produit des deux floats
+* @return float la division des deux floats
 */
+
+
+// Addition et soustraction
 
 $a = readline("Entrez un entier : ");
 $b = readline("Entrez un entier : ");
@@ -15,8 +23,34 @@ function getSum(int $a, int $b) : int
 {
     return $a + $b;
 }
-// test de la fonction
-echo "La somme de " . $a . "et de" . $b . "est de: ".getSum($a, $b).PHP_EOL;  
 
+function getSub(int $a, int $b) : int
+{
+    return $a - $b;
+}
+
+// test de la fonction
+echo "La somme de " . $a . " et de " . $b . " est de: " . getSum($a, $b) . PHP_EOL;
+echo "La soustraction de " . $a . " et de " . $b . " est de: " . getSub($a, $b) . PHP_EOL;
+
+// Multiplication et division
+$c = readline("Entrez un nombre à virgule : ");
+$d = readline("Entrez un nombre à virgule : ");
+function getMul(float $c, float $d) : float
+{
+    return $c * $d;
+}
+function getDiv(float $c, float $d) : float
+
+{
+    if ($d == 0.0) {
+        echo "Erreur : Division par zéro.".PHP_EOL;
+        exit(1);
+    }
+    return $c / $d;
+}
+// test de la fonction
+echo "Le produit de " . $c . " et de " . $d . " est de: " . getMul($c, $d) . PHP_EOL;
+echo "La division de " . $c . " et de " . $d . " est de: " . getDiv($c, $d) . PHP_EOL;
 
 ?>
