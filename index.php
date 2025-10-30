424ba2252fb5ce2fbf10ab70c8063c9a650f84de
f-gehin-crm <183800959+f-gehin-crm@users.noreply.github.com>
2025-10-30 14:54:40 +0100

    structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>calcul</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+    
+    require "vues/formulaire.php";
+    require "model/Contribuable.php";
+    ?>
+</body>
+</html>
---
+++ vues/formulaire.php
@@ -0,0 +1,9 @@
+<?php
+$contribuable = new Contribuable($item1, $item2);
+$impot = $contribuable->calculImpot();
+
+?>
+<form  method="GET">
+
+
+</form>
